3f3af1871b7d1677c1a7da9514b2c9c88f995d1a
Mauro Alfonso <maurogalfonso@gmail.com>
2022-05-19 21:44:40 -0300

    Uso de knex para base de datos

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const fs = require("fs");
+const express = require("express");
+const knex = require("./db");
 
 const app = express();
 
 // ARCHIVOS ESTATICOS
-app.use(express.static('views/layouts'));
+app.use(express.static("views/layouts"));
 
 // ARREGLO DE CHAT
 const chat = [];
 
-
 // SERVER
-const http = require("http")
+const http = require("http");
 const server = http.createServer(app);
 
 // PUERTO
 const port = process.env.PORT || 8080;
 
-
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.set("views", "./views")
-app.set("view engine", "hbs")
-
+app.set("views", "./views");
+app.set("view engine", "hbs");
 
 // Configuracion de handlebars
-app.engine( "hbs", handlebars.engine({
-    extname: ".hbs",
-    defaultLayout: "index",
-    layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials"
-}));
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: ".hbs",
+        defaultLayout: "index",
+        layoutsDir: __dirname + "/views/layouts",
+        partialsDir: __dirname + "/views/partials",
+    })
+);
 
 // SERVER SOCKET
 const { Server } = require("socket.io");
@@ -44,111 +43,91 @@ io.on("connection", (socket) => {
     //AGREGO NUEVO PRODUCTO
     socket.on("newproduct", (data) => {
         let newPr = data;
-        fs.readFile("./data/productos.json", "utf-8", (err, data) =>{
-        if (err) {
-            console.log("Error al leer el archivo")
-        } else {
-            let array  = JSON.parse(data);
-            let id = array.length + 1;
-            newPr.id = id;
-            array.push(newPr);
-            fs.writeFile("./data/productos.json", JSON.stringify(array), (err) => {
-                if (err) {
-                    console.log("Error al escribir el archivo")
-                } else {
-                    console.log("Producto agregado")
-                }
+        knex("products")
+            .insert(newPr)
+            .then(() => {
+                console.log("Producto agregado");
+                io.sockets.emit("productAdded", newPr);
+                return false;
             })
-            io.sockets.emit("productAdded", newPr);
-            return false;
-        }})
-    })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+
     socket.on("newMsg", (data) => {
         let newMsg = data;
-        fs.readFile("./data/chat.json", "utf-8", (err, data) =>{
-        if (err) {
-            console.log("Error al leer el archivo")
-        } else {
-            if (data == "") {
-                let array = [];
-                array.push(newMsg);
-                fs.writeFile("./data/chat.json", JSON.stringify(array), (err) => {
-                    if (err) {
-                        console.log("Error al escribir el archivo")
-                    } else {
-                        console.log("Mensaje agregado")
-                    }
-                }
-                )
+        knex("chat")
+            .insert(newMsg)
+            .then(() => {
+                console.log("Mensaje agregado");
                 io.sockets.emit("newMsg", newMsg);
                 return false;
-            } else {
-            let array  = JSON.parse(data);
-            array.push(newMsg);
-            fs.writeFile("./data/chat.json", JSON.stringify(array), (err) => {
-                if (err) {
-                    console.log("Error al escribir el archivo")
-                } else {
-                    console.log("Mensaje agregado")
-                }
             })
-            io.sockets.emit("newMsg", newMsg);
-            return false;
-        }}})
-        
-    })
-})
-
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+});
 
 // RUTA PRINCIPAL
 
-app.get("/", (req, res)=> {
-    fs.readFile("./data/productos.json", "utf-8", (err, data) =>{
-        if (err) {
-            res.send("Error al leer el archivo")
-        } else {
-            let array  = JSON.parse(data); // ARRAY DE PRODUCTOS
-            fs.readFile("./data/chat.json", "utf-8", (err, data) =>{
-                if (err) {
-                    res.send("Error al leer el archivo")
-                } else {
-                    if(data.length > 0){
-                        let chat = JSON.parse(data);    // ARRAY DE CHAT
-                        res.render("products", {data:array, chat:chat});           
-                    } else {
-                        res.render("products", {data:array});
-                    }
-                }
-            })
-            }           
-    })
-})
+app.get("/", (req, res) => {
+    knex
+        .select()
+        .table("products")
+        .then((data) => {
+            let array = data;
+            knex
+                .select()
+                .table("chat")
+                .then((data) => {
+                    let chat = data;
+                    res.render("products", { data: array, chat: chat });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
+// ELIMINAR PRODUCTO
 
+app.delete("/delete/:id", (req, res) => {
+    knex
+        .select()
+        .table("products")
+        .where("id", req.params.id)
+        .del()
+        .then((data) => {
+            console.log("Producto eliminado");
+            res.send("Producto eliminado");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
-app.post("/newproduct", (req, res) => {
-    let newPr = req.body;
-    fs.readFile("./data/productos.json", "utf-8", (err, data) =>{
-        if (err) {
-            res.send("Error al leer el archivo")
-        } else {
-            let array  = JSON.parse(data);
-            let id = array.length + 1;
-            newPr.id = id;
-            array.push(newPr);
-            fs.writeFile("./data/productos.json", JSON.stringify(array), (err) => {
-                if (err) {
-                    res.send("Error al escribir el archivo")
-                } else {
-                    res.redirect("/getall")
-                }
-            })
-        }
-    }
-    )
-})
+// ACTUALIZAR PRODUCTO
 
+app.put("/update/:id", (req, res) => {
+    knex
+        .select()
+        .table("products")
+        .where("id", req.params.id)
+        .update(req.body)
+        .then((data) => {
+            console.log("Producto actualizado");
+            res.send("Producto actualizado");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 server.listen(port, () => {
-    console.log('Server is running on port ' + port);
+    console.log("Server is running on port " + port);
 });
