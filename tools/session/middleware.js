eea167819fc648209f62873592771bc9aa5903e7
Mauro Alfonso <maurogalfonso@gmail.com>
2022-10-19 15:22:20 -0300

    Subida a heroku

--- tools/session/middleware.js
+++ tools/session/middleware.js
@@ -1,24 +1,24 @@
 import session from "express-session";
 import MongoStore from "connect-mongo";
 
+const expTime = Number(process.env.SESSION_EXPIRATION);
 
-const sessionMiddleware = 
-    session({
-        secret: "secret",
-        cookie: { maxAge: 600000 },
-        resave: true,
-        rolling: true,
-        saveUninitialized: true,
-        store: MongoStore.create({
-            mongoUrl: process.env.MONGODB_ATLAS_CLUSTER,
-            mongoOptions: {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-        }),
-        cookie: {
-            maxAge: 600000,
-        },
-    });
+const sessionMiddleware = session({
+  secret: "secret",
+  cookie: { maxAge: expTime },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_ATLAS_CLUSTER,
+    mongoOptions: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  }),
+  cookie: {
+    maxAge: expTime,
+  },
+});
 
 export default sessionMiddleware;
