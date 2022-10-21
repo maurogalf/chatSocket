6fd20746d2c7df38f9eecad96de4c94c9dfbe07f
Mauro Alfonso <maurogalfonso@gmail.com>
2022-10-21 17:12:50 -0300

    Intentando enviar el usuario en el header de la respuesta

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -4,7 +4,8 @@ class JwtMiddleware {
   tokenVerify(req, res, next) {
     try {
       const token = req.headers.authorization.split(" ")[1];
-      const result = jwtVerify(token);
+      const response = jwtVerify(token);
+      res.setHeader("user", response.user);
       next();
     } catch (err) {
       res.status(401).send({ Error: err.message });
