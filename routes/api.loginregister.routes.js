83aed1de66cc5e295bcc7d2026694fda8bf93eb2
Mauro Alfonso <maurogalfonso@gmail.com>
2022-10-24 16:41:27 -0300

    Se agrego la ruta para validar token

--- routes/api.loginregister.routes.js
+++ routes/api.loginregister.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { apiUsersController } from "../controllers/api/api.users.controller.js";
-import { jwtMiddleware } from "../middleware/jwt.middleware.js";
 import passport from "../tools/passport/local-auth.js";
 
 const apiLoginRegister = new Router();
@@ -12,5 +11,7 @@ apiLoginRegister.post(
 );
 
 apiLoginRegister.post("/register");
+
+apiLoginRegister.get("/validatetoken", apiUsersController.checkToken);
 
 export default apiLoginRegister;
